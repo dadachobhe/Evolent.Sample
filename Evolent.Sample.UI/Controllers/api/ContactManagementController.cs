2a6286e89c63488718f4e7841394efcb69380c28
dadachobhe <39794820+dadachobhe@users.noreply.github.com>
2018-06-01 13:00:53 +0530

    adding scripts and controller

---
+++ Evolent.Sample.UI/Controllers/api/ContactManagementController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Evolent.Sample.Core.Data;
+using Evolent.Sample.Data;
+
+namespace Evolent.Sample.UI.Controllers.Api
+{
+    public class ContactManagementController : ApiController
+    {
+         readonly Repository<Contact> _contactRepository;
+        private readonly UnitOfWork _unitOfWork = new UnitOfWork();
+
+        public ContactManagementController()
+        {
+            _contactRepository = _unitOfWork.Repository<Contact>();
+        }
+
+        [HttpGet]
+        public HttpResponseMessage Get()
+        {
+            IEnumerable<Contact> contactDetail;
+            HttpResponseMessage response;
+            try
+            {
+                contactDetail = _contactRepository.Get();
+                response = Request.CreateResponse(HttpStatusCode.OK, contactDetail);
+            }
+            catch (ApplicationException ex)
+            {
+                response = Request.CreateResponse(HttpStatusCode.BadRequest, new StringContent(ex.Message));
+            }
+            catch (Exception ex)
+            {
+                response = Request.CreateResponse(HttpStatusCode.BadGateway, new StringContent(ex.Message));
+            }
+
+            return response;
+        }
+
+        [HttpGet]
+        public HttpResponseMessage Get(int id)
+        {
+           Contact contactDetail;
+            HttpResponseMessage response;
+            try
+            {
+                contactDetail = _contactRepository.GetById(id);
+                response = Request.CreateResponse(HttpStatusCode.OK, contactDetail);
+            }
+            catch (ApplicationException ex)
+            {
+                response = Request.CreateResponse(HttpStatusCode.BadRequest, new StringContent(ex.Message));
+            }
+            catch (Exception ex)
+            {
+                response = Request.CreateResponse(HttpStatusCode.BadGateway, new StringContent(ex.Message));
+            }
+
+            return response;
+        }
+
+
+        [HttpPost]
+        public HttpResponseMessage Post(Contact contact)
+        {
+            HttpResponseMessage response;
+            try
+            {
+                contact.CreatedDate = DateTime.Now;
+                contact.CreatedBy = "admin";
+                _contactRepository.Insert(contact);
+                response = Request.CreateResponse(HttpStatusCode.OK, new StringContent("Record inserted successfully"));
+            }
+            catch (ApplicationException ex)
+            {
+                response = Request.CreateResponse(HttpStatusCode.BadRequest, new StringContent(ex.Message));
+            }
+            catch (Exception ex)
+            {
+                response = Request.CreateResponse(HttpStatusCode.BadGateway, new StringContent(ex.Message));
+            }
+            return response;
+        }
+
+        [HttpPut]
+        public HttpResponseMessage Put(Contact contact)
+        {
+            HttpResponseMessage response;
+            try
+            {
+                contact.UpdatedDate = DateTime.Now;
+                _contactRepository.Update(contact);
+
+                response = Request.CreateResponse(HttpStatusCode.OK, new StringContent("Record updated successfully"));
+            }
+            catch (ApplicationException ex)
+            {
+                response = Request.CreateResponse(HttpStatusCode.BadRequest, new StringContent(ex.Message));
+            }
+            catch (Exception ex)
+            {
+                response = Request.CreateResponse(HttpStatusCode.BadGateway, new StringContent(ex.Message));
+            }
+            return response;
+        }
+
+        [HttpDelete]
+        public HttpResponseMessage Delete(int id)
+        {
+            HttpResponseMessage response;
+            try
+            {
+                Contact contact = _contactRepository.GetById(id);
+                _contactRepository.Delete(contact);
+                response = Request.CreateResponse(HttpStatusCode.OK, new StringContent("Record deleted successfully"));
+            }
+            catch (ApplicationException ex)
+            {
+                response = Request.CreateResponse(HttpStatusCode.BadRequest, new StringContent(ex.Message));
+            }
+            catch (Exception ex)
+            {
+                response = Request.CreateResponse(HttpStatusCode.BadGateway, new StringContent(ex.Message));
+            }
+
+            return response;
+        }  
+    }
+}
---
+++ Evolent.Sample.UI/Scripts/contactmanagement.js
@@ -0,0 +1,138 @@
+﻿
+
+var ContactDetails = {
+
+    GetContactData: function () {
+        $("#list").jqGrid({
+            url: 'api/ContactManagement',
+            datatype: 'json',
+            mtype: 'Get',
+            colModel: [
+                {
+                    key: true, hidden: true, name: 'Id', index: 'Id', editable: false
+                },
+
+                {
+                   key: false, name: 'FirstName', index: 'FirstName', width: 245, editable: true, editrules: { required: true },
+                   editoptions: {
+                        maxlength: "255"
+                  }
+               },
+                {
+                    name: 'LastName', index: 'LastName', width: 245, editable: true, editrules: { required: true },
+                    editoptions: {
+                        maxlength: "255"
+                        }
+                },
+                {
+                    name: 'Email', index: 'Email', width: 245, editable: true, editrules: { regex: /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, required: true },
+                    editoptions: {maxlength: "50"}
+                    
+                },
+
+                {
+                    name: 'PhoneNumber', index: 'PhoneNumber', width: 145, editable: true, editrules: { required: true },
+                    editoptions: {
+                        maxlength: "10", dataInit: function (element) {
+                            $(element).keypress(function (e) {
+                                if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) {
+                                    alert("Accept only numeric value and only ten digits");
+                                    return false;
+                                }
+                            });
+                        }
+                    }
+                },
+                {
+                   name: 'Status', index: 'Status', width: 100, editable: true, editrules: { required: true }
+                }
+            ],
+            pager: jQuery('#pager'),
+            rowNum: 10,
+            loadonce: true,
+            rowList: [10, 20, 30, 40],
+            height: '100%',
+            viewrecords: true,
+            caption: 'Contact Details',
+            emptyrecords: 'No records to display',
+            jsonReader: {
+                repeatitems: false,
+                root: function (obj) { return obj; },
+                page: "page",
+                total: "total",
+                records: "records",
+                ContactNo: "0"
+            },
+            autowidth: true,
+            multiselect: false
+        }).navGrid('#pager', { add: false, edit: true, del: true, search: false, refresh: true },
+       {
+           // edit options  
+           zIndex: 1000,
+           url: 'api/ContactManagement/Put',
+           closeOnEscape: true,
+           closeAfterEdit: true,
+           recreateForm: true,
+           loadonce: true,
+           align: 'center',
+           afterComplete: function (response) {
+               GetContactData();
+               if (response.responseText) {
+
+                   alert(response.responseText);
+               }
+           }
+       }, {},
+       {
+           // delete options  
+           zIndex: 1000,
+           url: 'api/ContactManagement/Delete',
+           closeOnEscape: true,
+           closeAfterdel: true,
+           recreateForm: true,
+           msg: "Are you sure you want to delete this contact?",
+           afterComplete: function (response) {
+               if (response.responseText) {
+                   $("#alert-Grid").html("<b>" + response.responseText + "</b>");
+                   $("#alert-Grid").show();
+                   $("#alert-Grid").delay(3000).fadeOut("slow");
+               }
+           }
+       });
+    },
+    insertContactDetails: function () {
+
+
+        $("#btnSubmit").click(function () {
+
+            $.ajax(
+                {
+                    type: "POST", //HTTP POST Method    
+                    url: "api/ContactManagement", // Controller/View     
+                    data: { //Passing data    
+
+                        FirstName: $("#txtFName").val(), //Reading text box values using Jquery     
+                        LastName: $("#txtLName").val(),
+                        Email: $("#txtEmail").val(),
+                        PhoneNumber: $("#txtMobileNo").val(),
+                        Status: $("#status").val()
+                    },
+                    success: function (data) {
+                        //alert(data);
+                        $("#alert-danger").html("<b>" + data + "</b>");
+                        $("#alert-danger").show();
+                        $("#alert-danger").delay(10000).fadeOut("slow");
+                    },
+                    error: function (xhr, ajaxOptions, thrownError) {
+                        //var r = data.responseText;  
+                        //var errorMessage = r.Message;  
+                        $("#alert-danger").html("<b>" + data + "</b>");
+                        $("#alert-danger").show();
+                        $("#alert-danger").delay(10000).fadeOut("slow");
+                        GetContactData();
+                    }
+                });
+        });
+       
+    }
+}
